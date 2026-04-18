5b81613c64b135064bf245d22f5c7ab676eb3294
Ryuzora <ryuzora8@gmail.com>
2026-04-18 19:14:53 +0800

    Migrating to User Site URL

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 1. Tells Next.js to build a static HTML folder instead of a server app
   output: 'export',
-
-  // 2. Disables the Next.js image server (required for GitHub Pages)
   images: {
     unoptimized: true,
   },
